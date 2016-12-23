70f81bf6adc706a3ee0f55dc3a5251d290b2cc85
Jack Truong <jack.truong@uwaterloo.ca>
2016-12-23 16:07:00 -0500

    Functioning update system implemented, still need to remove UW specific URLs.

--- buildinit.sh
+++ buildinit.sh
@@ -4,7 +4,7 @@ INIT=$(mktemp -d)
 
 cd $INIT
 
-mkdir -p {bin,dev,etc,lib,lib64,proc,root,sbin,sys,sdcard,squashfs,upper,overlay}
+mkdir -p {bin,dev,etc,lib,lib/arm-linux-gnueabihf,lib64,proc,root,sbin,sys,sdcard,squashfs,upper,overlay}
 cp -a /dev/{null,console,tty,mmcblk0,ttyAMA0,ttyS0} ./dev/
 cp $CWD/initscript ./init
 chmod +x ./init
@@ -16,6 +16,13 @@ apt-get download busybox-static
 dpkg -x *busybox-static*deb ./
 rm *busybox-static*deb
 ./bin/busybox --install ./bin/
+
+cp /lib/arm-linux-gnueabihf/ld-linux-armhf.so.3 ./lib/arm-linux-gnueabihf/ld-linux-armhf.so.3
+cp /lib/arm-linux-gnueabihf/libc.so.6 ./lib/arm-linux-gnueabihf/libc.so.6
+cp /lib/arm-linux-gnueabihf/libnss_dns.so.2 ./lib/arm-linux-gnueabihf/libnss_dns.so.2
+cp /lib/arm-linux-gnueabihf/libnss_files.so.2 ./lib/arm-linux-gnueabihf/libnss_files.so.2
+cp /lib/arm-linux-gnueabihf/libnss_mdns4_minimal.so.2 ./lib/arm-linux-gnueabihf/libnss_mdns4_minimal.so.2
+cp /lib/arm-linux-gnueabihf/libresolv.so.2 ./lib/arm-linux-gnueabihf/libresolv.so.2
 
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > $CWD/enginfo.gz
 
